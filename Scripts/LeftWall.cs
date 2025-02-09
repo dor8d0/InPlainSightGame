8b76ad671db1e60a6b03c161b57604bacae865de
dor8d0 <troyw792@gmail.com>
2025-02-09 12:11:43 -0600

    Adds wall image

--- Scripts/LeftWall.cs
+++ Scripts/LeftWall.cs
@@ -12,4 +12,10 @@ public partial class LeftWall : RigidBody2D
 	public override void _Process(double delta)
 	{
 	}
+	
+	void _on_body_entered(Node body){
+		if(body is Enemy enemy){
+			enemy.QueueFree();
+		}
+	}
 }
