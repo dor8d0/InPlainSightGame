6d2cde040b3a8aa1d74b5253650737b4631eb6f4
dor8d0 <troyw792@gmail.com>
2025-02-09 12:35:31 -0600

    Adds hiding spots

--- Scripts/HidingSpot.cs
+++ Scripts/HidingSpot.cs
@@ -5,11 +5,15 @@ public partial class HidingSpot : Area2D
 {
 	public bool playerCanHide = false;
 	public Player player;
+	Sprite2D awaitingHide;
+	Sprite2D hiding;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = GetParent().GetNode<Player>("Player");
+		awaitingHide = this.GetNode<Sprite2D>("AwaitingHide");
+		hiding = this.GetNode<Sprite2D>("Hiding");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +22,15 @@ public partial class HidingSpot : Area2D
 		if(Input.IsActionJustPressed("Hide") && playerCanHide){
 			if(!player.isHiding){
 				player.isHiding = true;
-				player.Visible = false;
+				player.GetNode<Node2D>("Sprite2D").Visible = false;
+				hiding.Visible = true;
+				awaitingHide.Visible = false;
 			}
 			else{
 				player.isHiding = false;
-				player.Visible = true;
+				player.GetNode<Node2D>("Sprite2D").Visible = true;
+				hiding.Visible = false;
+				awaitingHide.Visible = true;
 			}
 		}
 	}
